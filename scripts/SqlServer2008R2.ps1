49f1148d2ec2ded8e039bdbacf9d005de5f4b5ae
Luke Carrier <luke@carrier.im>
2015-04-06 19:24:56 +0100

    Clean up

--- scripts/SqlServer2008R2.ps1
+++ scripts/SqlServer2008R2.ps1
@@ -14,7 +14,7 @@ $CacheDir  = (Join-Path (Split-Path $ScriptDir) "cache")
 echo "Installing .NET Framework 3.5..."
 Add-WindowsFeature as-net-framework
 
-echo "Installing SQL Server Express 2008 R2..."
+echo "Installing SQL Server 2008 R2 SP2 - Express Edition..."
 $InstallerPath = (Join-Path $CacheDir "SQLEXPRADV_x64_ENU.exe")
 $ConfigurationFilePath = (Join-Path $ScriptDir "SqlServer2008R2ConfigurationFile.ini")
 &$InstallerPath /ACTION=install /ConfigurationFile=$ConfigurationFilePath `
