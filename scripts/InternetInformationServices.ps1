e259a1efab29a0ef257fb1c0954238547152a8f2
Luke Carrier <luke@carrier.im>
2015-04-19 14:55:45 +0100

    Bunch of ugly kludge to remove the default site and unfuck IIS afterwards

--- scripts/InternetInformationServices.ps1
+++ scripts/InternetInformationServices.ps1
@@ -22,5 +22,35 @@ Import-Module ServerManager
     "Web-Scripting-Tools"
 )
 
+# Base paths
+$IisAppPoolPath = (Join-Path "IIS:" "AppPools")
+$IisWebSitePath = (Join-Path "IIS:" "Sites")
+
+# Defaults
+$DefaultAppPoolName         = "DefaultAppPool"
+$DefaultAppPoolPath         = (Join-Path $IisAppPoolPath $DefaultAppPoolName)
+$DefaultWebSiteName         = "Default Web Site"
+$DefaultWebSitePath         = (Join-Path $IisWebSitePath $DefaultWebSiteName)
+$DefaultWebSitePhysicalPath = (Join-Path (Join-Path "C:" "inetpub") "wwwroot")
+
 echo "Installing IIS..."
 Add-WindowsFeature -Name $Features | Out-Null
+
+# Incremental site ID generation fails if no sites exist, so deleting the
+# default site will actually break the WebAdministration module. Disable
+# incremental site ID generation because it seems like a stupid idea anyway.
+#
+# such enterprise. very money. wow.
+# http://forums.iis.net/post/1912421.aspx
+echo "Disabling incremental site ID generation..."
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Inetmgr\Parameters `
+                 -Name IncrementalSiteIDCreation -Value 0
+
+# Delete the default junk
+echo "Removing default pool and site (if present)..."
+if (Test-Path $DefaultAppPoolPath) {
+    Remove-WebAppPool $DefaultAppPoolName
+}
+if (Test-Path $DefaultWebSitePath) {
+    Remove-Website $DefaultWebSiteName
+}
