0dfccdb24a2957253c86cafbf8ea386655c2995d
Luke Carrier <luke@carrier.im>
2015-06-23 17:36:43 +0100

    Attempt Windows Server activation
    It seems as though Server edition evaluation copies ship with a product
    key preinstalled, so triggering Windows Activation without installing a
    product key should be sufficient for activation.

--- scripts/ActivateWindows.ps1
+++ scripts/ActivateWindows.ps1
@@ -9,14 +9,11 @@
 # 180 day trial product keys:
 #
 # Edition      Key
+# ----------   -----------------------------
 # Web          KBV3Q-DJ8W7-VPB64-V88KG-82C49
 # Standard     4GGC4-9947F-FWFP3-78P6F-J9HDR
 # Enterprise   7PJBC-63K3J-62TTK-XF46D-W3WMD
 # Datacenter   QX7TD-2CMJR-D7WWY-KVCYC-6D2YT
-$ProductKey = "4GGC4-9947F-FWFP3-78P6F-J9HDR"
 
 Write-Host "Activating Windows..."
-$Service = Get-WmiObject -ComputerName $env:ComputerName `
-                         -Query "SELECT * FROM SoftwareLicensingService"
-$Service.InstallProductKey($ProductKey)
-$Service.RefreshLicenseStatus()
+&slmgr.vbs /ato
